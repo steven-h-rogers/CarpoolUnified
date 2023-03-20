f895f97de5d819794e1cf8d4ab1cf64d6098dbca
mayachakra <mayajchakravarty@gmail.com>
2023-03-19 23:50:45 -0400

    restructed files

--- src/Job.java
+++ src/Job.java
@@ -50,43 +50,13 @@ public class Job {
 
     }
 
-    public static List<Map<String , String>> calculateJobDurationTimes(String job_filename) throws IOException {
-        ConvertTextToHashmapList jobs = new ConvertTextToHashmapList(job_filename);
-
-        List<Map<String , String>> jobCompletionTimeList  = new ArrayList<Map<String,String>>();
-
-        for (int i = 0; i < jobs.MapList.size(); i++){
-            Map<String , String> jobEntry = jobs.MapList.get(i);
-
-            int jobEntryCompletionTime = 0;
-            if (i == 0) {
-                jobEntryCompletionTime = Integer.parseInt(jobEntry.get("JobDuration"));
-            } else {
-                Map<String , String> previousJobEntry = jobs.MapList.get(i - 1);
-                jobEntryCompletionTime = Integer.parseInt(previousJobEntry.get("JobCompletionTime"))
-                        + Integer.parseInt(jobEntry.get("JobDuration"));
-            }
-
-            jobEntry.put("JobCompletionTime", "" + jobEntryCompletionTime);
-
-            jobCompletionTimeList.add(jobs.MapList.get(i));
-        }
-
-        return jobCompletionTimeList;
-    }
-
     void deliver(){
 
     }
 
 
     public static void main (String[] args) throws IOException {
-        List<Map<String, String>> jobCompletionTimes = calculateJobDurationTimes("jobs.txt");
 
-
-        for (Map<String, String>  job: jobCompletionTimes){
-            System.out.println(job);
-        }
     }
 
 }
